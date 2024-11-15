75f847d17a19e5c08066a2c24ac88479c9e41941
natiorozco <nataliaorozcodelgado@gmail.com>
2024-11-15 09:43:36 -0600

    special tile

--- main/src/main/java/org/abno/board/SpecialTile.java
+++ main/src/main/java/org/abno/board/SpecialTile.java
@@ -6,12 +6,15 @@ import org.abno.players.PlayerData;
 
 public class SpecialTile extends Tile{
 
+    Game game;
 
-    public SpecialTile(int id, String imgRoute){
+    public SpecialTile(int id, String imgRoute, Game game){
         super(id, imgRoute);
+        this.game = game;
     }
 
-    public void interact(PlayerData player){;
+    public void interact(PlayerData player){
+        game.play(player);
     }
 
     public Game getGame() {
