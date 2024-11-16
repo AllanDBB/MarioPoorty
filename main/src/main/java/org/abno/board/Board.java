70cf59e4c74d5ff5acdafe59d53c89b17399b57b
Allan Bolaños <adbyb.es@gmail.com>
2024-11-15 18:40:33 -0600

    Manual order for board

--- main/src/main/java/org/abno/board/Board.java
+++ main/src/main/java/org/abno/board/Board.java
@@ -31,103 +31,69 @@ public class Board {
             tiles.add(new SpecialTile(id, imgRoute));
         }*/
 
-        tiles.add(new SpecialTile(0, "SpecialTile.png", new CatchTheCat()));
-        tiles.add(new SpecialTile(1, "SpecialTile.png", new CatchTheCat()));
-
         tiles.add(new SpecialTile(2, "SpecialTile.png", new CatchTheCat()));
-        tiles.add(new SpecialTile(3, "SpecialTile.png", new CatchTheCat()));
-
-        tiles.add(new SpecialTile(4, "SpecialTile.png", new CatchTheCat()));
         tiles.add(new SpecialTile(5, "SpecialTile.png", new CatchTheCat()));
-
-        tiles.add(new SpecialTile(6,  "SpecialTile.png", new CatchTheCat()));
+        tiles.add(new SpecialTile(6, "SpecialTile.png", new CatchTheCat()));
         tiles.add(new SpecialTile(7, "SpecialTile.png", new CatchTheCat()));
-
         tiles.add(new SpecialTile(8, "SpecialTile.png", new CatchTheCat()));
-        tiles.add(new SpecialTile(9, "SpecialTile.png", new CatchTheCat()));
-
         tiles.add(new SpecialTile(10, "SpecialTile.png", new CatchTheCat()));
-        tiles.add(new SpecialTile(11, "SpecialTile.png", new CatchTheCat()));
-
-        //estas son las de sockets pero mientras tanto
-        tiles.add(new SpecialTile(12, "SpecialTile.png", new CatchTheCat()));
-        tiles.add(new SpecialTile(13, "SpecialTile.png", new CatchTheCat()));
+        tiles.add(new SpecialTile(12,  "SpecialTile.png", new CatchTheCat()));
         tiles.add(new SpecialTile(14, "SpecialTile.png", new CatchTheCat()));
-        tiles.add(new SpecialTile(15, "SpecialTile.png", new CatchTheCat()));
         tiles.add(new SpecialTile(16, "SpecialTile.png", new CatchTheCat()));
-        tiles.add(new SpecialTile(17, "SpecialTile.png", new CatchTheCat()));
-        tiles.add(new SpecialTile(18, "SpecialTile.png", new CatchTheCat()));
-
-        /*for (int i = 0; i < 10; i++) {
-            int id = i + 19;
-            String imgRoute = "path/to/evil_tile_image_" + id + ".png";
-            tiles.add(new EvilTile(id, imgRoute));
-        }*/
+        tiles.add(new SpecialTile(21, "SpecialTile.png", new CatchTheCat()));
+        tiles.add(new SpecialTile(27, "SpecialTile.png", new CatchTheCat()));
+        tiles.add(new SpecialTile(29, "SpecialTile.png", new CatchTheCat()));
+        tiles.add(new SpecialTile(32, "SpecialTile.png", new CatchTheCat()));
+        tiles.add(new SpecialTile(33, "SpecialTile.png", new CatchTheCat()));
+        tiles.add(new SpecialTile(34, "SpecialTile.png", new CatchTheCat()));
+        tiles.add(new SpecialTile(35, "SpecialTile.png", new CatchTheCat()));
+        tiles.add(new SpecialTile(36, "SpecialTile.png", new CatchTheCat()));
+        tiles.add(new SpecialTile(37, "SpecialTile.png", new CatchTheCat()));
+        tiles.add(new SpecialTile(35, "SpecialTile.png", new CatchTheCat()));
 
         tiles.add(new FirstTube(19, "FirstTube.png")); //FirstTube
-        tiles.add(new Jail(20, "Jail.png")); //Jail
-        tiles.add(new IceFlower(21, "IceFlower.png")); //IceFlower
-        tiles.add(new FireFlower(22, "FireFlower.png")); //FireFlower
-        tiles.add(new Star(23, "Star.png")); //Star
-        tiles.add(new Tail(24, "Tail.png")); //Tail
-        tiles.add(new Jail(25, "Jail.png")); //Jail
+        tiles.add(new Jail(13, "Jail.png")); //Jail
+        tiles.add(new IceFlower(30, "IceFlower.png")); //IceFlower
+        tiles.add(new FireFlower(3, "FireFlower.png")); //FireFlower
+        tiles.add(new Star(17, "Star.png")); //Star
+        tiles.add(new Tail(20, "Tail.png")); //Tail
+        tiles.add(new Jail(22, "Jail.png")); //Jail
         tiles.add(new SecondTube(26, "SecondTube.png")); //SecondTube
-        tiles.add(new Star(27, "Star.png")); //Star
+        tiles.add(new Star(1, "Star.png")); //Star
         tiles.add(new ThirdTube(28, "ThirdTube.png")); //ThirdTube
 
+        tiles.add(new Tile(0, "NormalTile.png"));
+        tiles.add(new Tile(4, "NormalTile.png"));
+        tiles.add(new Tile(9, "NormalTile.png"));
+        tiles.add(new Tile(11, "NormalTile.png"));
+        tiles.add(new Tile(15, "NormalTile.png"));
+        tiles.add(new Tile(18, "NormalTile.png"));
+        tiles.add(new Tile(23, "NormalTile.png"));
+        tiles.add(new Tile(24, "NormalTile.png"));
+        tiles.add(new Tile(25, "NormalTile.png"));
+        tiles.add(new Tile(31, "NormalTile.png"));
 
-        for (int i = 0; i < (maxTiles - 18 - 10); i++) {
-            int id = i + 29;  // Continuamos el conteo desde 29
-            String imgRoute = "NormalTile.png";
-            tiles.add(new Tile(id, imgRoute));
-        }
-
-        shuffleBoard();
-
-        arrangeSpecialTiles();
+        sortTilesById();
     }
 
-    private void shuffleBoard() {
+    public void sortTilesById() {
+        // Iteramos por todas las fichas de la lista
         for (int i = 0; i < tiles.size(); i++) {
-            if (i == 19 || i == 26 || i == 28){
-                continue;
+            // Encontramos el índice del elemento con el ID más pequeño desde i hasta el final
+            int minIndex = i;
+
+            for (int j = i + 1; j < tiles.size(); j++) {
+                // Si encontramos un tile con un ID menor, actualizamos minIndex
+                if (tiles.get(j).getId() < tiles.get(minIndex).getId()) {
+                    minIndex = j;
+                }
             }
 
-            int j = random.nextInt(tiles.size());
-
-            while (j == 19 || j == 26 || j ==28){
-                j = random.nextInt(tiles.size());
-            }
-
-            Tile temp = tiles.get(i);
-            tiles.set(i, tiles.get(j));
-            tiles.set(j, temp);
-        }
-    }
-
-
-    public void arrangeSpecialTiles() {
-        placeTileAtPosition(19);
-        placeTileAtPosition(26);
-        placeTileAtPosition(28);
-
-
-        for (int i = 0; i < tiles.size(); i++) {
-            if (tiles.get(i).getId() != 19 && tiles.get(i).getId() != 26 && tiles.get(i).getId() != 28) {
-                tiles.get(i).setId(i);
-            }
-        }
-    }
-
-
-    private void placeTileAtPosition(int desiredPosition) {
-        for (int i = 0; i < tiles.size(); i++) {
-            if (tiles.get(i).getId() == desiredPosition) {
-
-                Tile temp = tiles.get(desiredPosition);
-                tiles.set(desiredPosition, tiles.get(i));
-                tiles.set(i, temp);
-                break;
+            // Si minIndex ha cambiado, intercambiamos las fichas
+            if (minIndex != i) {
+                Tile temp = tiles.get(i);
+                tiles.set(i, tiles.get(minIndex));
+                tiles.set(minIndex, temp);
             }
         }
     }
